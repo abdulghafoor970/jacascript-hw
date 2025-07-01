8bd37cf02a32e89d08bebfb9086881f8cc673cd7
abdulghafoor970 <imabdulghafoor9@gmail.com>
2025-07-01 15:16:56 +0500

    javascript work added

---
+++ aap.js
@@ -0,0 +1,80 @@
+
+// alert("welcome to javascript");
+
+//  prompt("enter your name here to continue")
+
+
+//  var userName= prompt(" what is your name  ");
+
+//  var age= prompt(" what is your age ");
+
+//  console.log("username is", userName);
+
+//  console.log("age is ",age);
+
+
+// var color = prompt("color");
+// alert( "wow!" + color + " this is a nice color" );
+
+//   var color = prompt("what is your favorite color ?");
+//      if (color === "red") {
+//         alert ("red is a great color for passion and energy ")
+// }
+// if (color === "blue") {
+//     alert ("blue is a calming color that represents trust and stability")
+
+// }
+// if (color === "green") {
+//     alert ("green is a refreshing color that symbolizes nateure and growth ")
+
+// }
+// if (color === "red and blue") {
+//     alert ("red and blue are commplementary colors that create a vibrant contrast")
+// 
+
+
+// var num1 = + prompt ("  enter your number");
+
+//  var num2= + prompt (" enter your number ");
+
+//  console.log( num1 + num2  )
+
+
+//  var num1 = +prompt("enter first number ");
+
+//  var num2 = +prompt("enter second number");
+
+//  console.log("your answer is" + (num1 + num2))
+
+
+ var userName = prompt ("what is your name ?");
+
+ var country = prompt ("what is your country name ?");
+
+ var hobby = prompt (" what is your hobby ?");
+
+ alert(" hi, i'm " + userName + ", from " + country + " my hobby is " + hobby + " nice to meet you !"
+
+
+    var street =prompt("what is your street ?");
+
+ var city = prompt("what is your city name ?");
+
+ var country = prompt(" waht is your country nhame ?");
+
+ alert("your address is "  +  street + "," +  city + ","  + country + "." );
+
+ console.log("your address is " +  street + "," +  city + ","  + country + "." )
+
+
+ var userName = prompt ("write  your  full name ?");
+
+ var number =prompt ("write any number ?");
+
+ console.log("your user name is " + userName +number );
+
+ var firstName = prompt ("write  your  first name ?");
+ var secondName = prompt ("write  your  second name ?");
+ console.log("your gmail is " + firstName + secondName + "@gmail.com" )
+
+
